3d3555b183003b97950f5966751c70267f28231e
trinhhuong <dat.nguyen@codegym.vn>
2026-09-22 21:13:46 +0700

    sử dụng session để matching dữ liệu

--- src/main/java/codegym/vn/controller/CustomerController.java
+++ src/main/java/codegym/vn/controller/CustomerController.java
@@ -5,12 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
     @GetMapping
-    public String getCustomers(Model model) {
+    public String getCustomers(Model model, HttpSession session) {
         model.addAttribute("userName", "12345");
+        model.addAttribute("today", "22 tháng 9 năm 2026");
+        session.setAttribute("user", Map.of( "name", "K", "age", 11));
         return "customer";
     }
 }
